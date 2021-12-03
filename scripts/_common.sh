faf2d9da1361cc342f8641a741ae7f9379f12554
tituspijean <tituspijean@outlook.com>
2021-12-03 21:14:34 +0100

    Install NodeJS and npm with the core helper

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,11 +5,13 @@
 #=================================================
 
 # dependencies used by the app
-pkg_dependencies="python3-venv expect nodejs npm \
+pkg_dependencies="python3-venv expect \
 apt-transport-https ca-certificates gnupg2 zlib1g-dev dumb-init gosu cron unzip curl \
 fontconfig fonts-ipafont-gothic fonts-wqy-zenhei fonts-thai-tlwg fonts-kacst fonts-symbola fonts-noto fonts-freefont-ttf \
 wget curl chromium git ffmpeg youtube-dl ripgrep \
 build-essential python-dev python3-dev"
+
+nodejs_version=14
 
 #=================================================
 # PERSONAL HELPERS
