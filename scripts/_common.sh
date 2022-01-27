2b96c2c2c0d043425079b088a2b5b7c3fded2308
yalh76 <yalh@yahoo.com>
2022-01-27 21:05:56 +0100

    Fix dependencies for bullseye

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -9,7 +9,7 @@ pkg_dependencies="python3-venv expect \
 apt-transport-https ca-certificates gnupg2 zlib1g-dev dumb-init gosu cron unzip curl \
 fontconfig fonts-ipafont-gothic fonts-wqy-zenhei fonts-thai-tlwg fonts-kacst fonts-symbola fonts-noto fonts-freefont-ttf \
 wget curl chromium git ffmpeg youtube-dl ripgrep \
-build-essential python-dev python3-dev"
+build-essential python3-dev"
 
 nodejs_version=14
 
