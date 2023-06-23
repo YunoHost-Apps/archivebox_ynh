d40064a9376eed3c0acf84fffc71a9e7ddc9d6a7
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2023-06-23 10:08:31 +0200

    v2

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,13 +4,6 @@
 # COMMON VARIABLES
 #=================================================
 
-# dependencies used by the app (must be on a single line)
-#REMOVEME? pkg_dependencies="python3-venv expect \
-apt-transport-https ca-certificates gnupg2 zlib1g-dev dumb-init gosu cron unzip curl \
-fontconfig fonts-ipafont-gothic fonts-wqy-zenhei fonts-thai-tlwg fonts-kacst fonts-symbola fonts-noto fonts-freefont-ttf \
-wget curl chromium git ffmpeg youtube-dl ripgrep \
-build-essential python3-dev"
-
 nodejs_version=14
 
 #=================================================
